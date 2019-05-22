2df6b12ac4b55561b199418e53b434e0e747c31e
github username <dekadekadeka@gmail.com>
2019-05-22 13:11:24 -0400

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,18 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build(recipe_id: @recipe)
+    @recipe.ingredients.build(recipe_id: @recipe)
   end
 
   def create
+    @recipe = Recipe.create(params_hash)
+    redirect_to recipe_path(@recipe)
+  end
+
+  private
+
+  def params_hash
+    params.require(:recipe).permit!
   end
 end
